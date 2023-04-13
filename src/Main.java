4c86d95c68e9eeb278cf07e89be4d787817d194d
totomaro3 <ppoiuy1451@gmail.com>
2023-04-13 19:40:48 +0900

    git 업로드 연습

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class Main {
+	public static void main(String[] args) {
+
+		Article article1 = new Article();
+		System.out.println(article1.id);
+	
+		Article article2 = new Article();
+		System.out.println(article2.id);
+	
+	
+	
+	
+	}
+}
