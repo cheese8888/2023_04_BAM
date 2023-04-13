95d1023d04aef2557c3e462dcf17f5a51b22c2d5
cheese8888 <chankoreait@gmail.com>
2023-04-13 20:50:49 +0900

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 class Main {
 	public static void main(String[] args) {
 
-		Article article1 = new Article();
-		System.out.println(article1.id);
-	
-		Article article2 = new Article();
-		System.out.println(article2.id);
+		kim chan joong
 	
 	
 	
