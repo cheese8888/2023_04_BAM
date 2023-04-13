3aaad43425eea82294fbb26068a011b2a2d6b233
cheese8888 <chankoreait@gmail.com>
2023-04-13 21:12:34 +0900

    chan1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 class Main {
 	public static void main(String[] args) {
-
-		kim chan joong
-	
-	
-	
-	
+		System.out.println("==프로그램 시작==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("명령어) ");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("exit")) {
+				break;
+			}
+		}
+		sc.close();
+		System.out.println("==프로그램 끝==");
 	}
 }
+
+		
